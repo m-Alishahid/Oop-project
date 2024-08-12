d2cb247ad5b0ea7893b65dd1071a876eed11c709
Muhammad Ali Shahid <160266513+m-Alishahid@users.noreply.github.com>
2024-08-12 09:02:06 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,68 @@
+#! /usr/bin/env node 
+//Oop project
+console.log(chalk.yellowBright("**********OOP Project**********(By:m.a.s)"))
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+class Student{
+   name:string
+   constructor(n:string){
+      this.name=n
+
+   }
+}
+class Person{
+   students:Student[]=[]
+   addStudent(obj:Student){
+      this.students.push(obj)
+   }
+}
+const person=new Person()
+const programStart=async(person:Person)=>{
+   do{ 
+    console.log("Welcome!");
+    const ans=await inquirer.prompt([{
+      name:"select",
+      type:"list",
+      message:"Whom would you like to interact with?",
+      choices:["Staff","Students","Exit"]
+    }])
+    if(ans.select=="Staff"){
+      console.log("You approach the staff room.please feel free to ask any question.")
+   }
+   else if(ans.select=="Students"){
+      const ans=await inquirer.prompt([{
+         name:"student",
+         type:"input",
+         message:" Enter the student name you wish to engaged with:"
+      }])
+      const student=person.students.find(val=>val.name==ans.student)
+
+      if(!student){
+         const name=new Student(ans.student)
+         person.addStudent(name);
+         console.log(`Hello I am ${name.name}.Nice to meet you!`);
+         console.log("New Student added")
+         console.log("Current student list:");
+         console.log(person.students);
+   
+      }
+   
+      else{
+         console.log(`Hello I am ${student.name}. Nice to see you again!`);
+         console.log("Existing students list:")
+         console.log(person.students);
+      }
+   
+   }
+
+   else if(ans.select=="Exit"){
+      console.log("Exiting the program...");
+      process.exit();
+   }
+      }while(true)
+}
+
+programStart(person)
+
+
